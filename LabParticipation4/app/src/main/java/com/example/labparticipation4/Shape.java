9934ddac7af50668ef88a4c9a6827e1d7a56f815
Rowan Tilroe <rtilroe@ualberta.ca>
2021-10-03 10:28:46 -0600

    conflict resolution

--- LabParticipation4/app/src/main/java/com/example/labparticipation4/Shape.java
+++ LabParticipation4/app/src/main/java/com/example/labparticipation4/Shape.java
@@ -1,7 +1,6 @@
 package com.example.labparticipation4;
 
 public abstract class Shape {
-    public String colour;
     public int x;
     public int y;
     public String color = "$PUT_YOUR_CHOICE_OF_COLOR";
